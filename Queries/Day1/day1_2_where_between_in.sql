72170741a65fe55e655ed1b1e0431928cdfa1e9d
suhedapehlivan <suheda.90@gmail.com>
2023-03-10 14:08:59 +0300

    day1 where between

--- Queries/Day1/day1_2_where_between_in.sql
+++ Queries/Day1/day1_2_where_between_in.sql
@@ -28,3 +28,29 @@ where SALARY>=5000 and SALARY<=10000;
 select FIRST_NAME,LAST_NAME,SALARY
 from EMPLOYEES
 where SALARY between 5000 and 10000;
+
+
+
+--get me all info where emp_id 130 to 170
+select *
+from EMPLOYEES
+where EMPLOYEE_ID between 130 and 170;
+
+
+--get me all info where emp_id 105 106 154 129
+select *
+from EMPLOYEES
+where EMPLOYEE_ID=105 or EMPLOYEE_ID=106 or EMPLOYEE_ID=154 or EMPLOYEE_ID=129;
+
+
+select *
+from EMPLOYEES
+where EMPLOYEE_ID in(105,106,154,129);
+
+
+
+--get me city of where country_id IT,US,UK
+select CITY
+from LOCATIONS
+where COUNTRY_ID in('IT','US','UK');
+

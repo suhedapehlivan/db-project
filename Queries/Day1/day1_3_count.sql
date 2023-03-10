bfb62c022ad418e7ea9102395801ce42be86acb5
suhedapehlivan <suheda.90@gmail.com>
2023-03-10 14:17:21 +0300

    day1 count

--- Queries/Day1/day1_3_count.sql
+++
@@ -1,8 +0,0 @@
---get me info who is working as IT_PROG or SA_REP
-
-select * from EMPLOYEES
-where JOB_ID in('IT_PROG','SA_REP');
-
---how many employee working as IT_PROG or SA_REP
-select count(*) from EMPLOYEES
-where JOB_ID in('IT_PROG','SA_REP');

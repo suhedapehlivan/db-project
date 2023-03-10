3cbf5547c0054bf57f7502658f17c47b23d9bb06
suhedapehlivan <suheda.90@gmail.com>
2023-03-10 18:31:08 +0300

    day1 group by & having

--- Queries/Day1/day1_4_Avg_groupBy.sql
+++ Queries/Day1/day1_4_Avg_groupBy.sql
@@ -7,7 +7,24 @@ select avg(SALARY)
 from EMPLOYEES
 where JOB_ID='IT_PROG';
 
---group by: istediğin kategori başlığını grup olarak ele almak için kullanılır
+--group by: is grouping the inputs, then implement the function each of the group(box)
+-- istediğin kategori başlığını grup olarak ele almak için kullanılır
+--group by ile tek sonuç alınacak şeyleri yapabilirsin;
+-- min max avg count sum
+
+select JOB_ID
+from EMPLOYEES
+group by JOB_ID;
+
+
 select JOB_ID, avg(SALARY),count(*),sum(SALARY)
 from EMPLOYEES
 group by JOB_ID;
+
+
+--having
+--get me job_ids where their avg salary is more than 5k
+select JOB_ID,avg(SALARY),count(*),sum(SALARY)
+from EMPLOYEES
+group by JOB_ID
+having avg(SALARY) >5000;
